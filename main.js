8a2f688b1174a641c834761cd4497b0f6b8b01ef
afrila <afrilabimaris@gmail.com>
2017-04-09 21:47:11 +0800

    Add files via upload
    Add sticky menu with JQUERY

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+$(function() {
+	var navigasi = $('nav').offset().top;
+	var sticky = function(){
+		var scrolltop = $(window).scrollTop();
 
+		if (scrolltop > navigasi) 
+		{
+			$('nav').addClass('fixed');
+		}else
+		{
+			$('nav').removeClass('fixed');
+		}
+	};
+	sticky();
+	$(window).scroll(function(){
+		sticky();
+	})
+})
